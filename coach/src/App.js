eae666dca587d49c9c024ee177dede9cf1988806
Shaurya-Srivastav <ssrivastav@ucdavis.edu>
2024-01-18 09:44:03 -0800

    groundwork for services and homepage created

---
+++ coach/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+       <section id="home">
+        <h1>Home</h1>
+        <p>Welcome to the Home section</p>
+      </section>
+
+
+      <section id="services">
+        <h1>Services</h1>
+        <p>Details about the services offered</p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
